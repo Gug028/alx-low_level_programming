db6c3e859a7823cbb3ef46f87352d2091a525c82
Gugu <Gug028>
2023-08-22 08:42:01 -0700

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -21,7 +21,7 @@ int main(void)
 		n /= 2;
 	}
 
-	for (i = 3; i <= sqrt(n);)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
@@ -32,6 +32,7 @@ int main(void)
 
 	if (n > 2)
 		max = n;
+
 	printf("%ld\n", max);
 
 	return (0);
